b82d6d0ec90186bdebd33775dffe83de134cc2a7
MrWhite <mayankbungla8@gmail.com>
2026-02-12 20:11:18 +0530

    modification in code

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -9,8 +9,13 @@ Select EngagementId,
 	ProductId,
 	Likes,
 	UPPER (replace(ContentType, 'Socialmedia','Social media')) as contentType,
-	LEFT ( ViewsClicksCombined, CHARINDEX ('-', ViewsClicksCombined)-1) as views,
+
+	--EXTRACT VIEWS: Finds the hyphen position and grabs all characters to the left of it.Example: '150-40' becomes '150'
+    LEFT ( ViewsClicksCombined, CHARINDEX ('-', ViewsClicksCombined)-1) as views, 
+
+	-- EXTRACT CLICKS: Finds the hyphen position. Total length of the string minus the position of the hyphen gives the number of characters to the right of the hyphen.
 	Right ( ViewsClicksCombined, Len(viewsclickscombined)- CHARINDEX ('-',ViewsClicksCombined)) as clicks,
+
 	format(convert(date, engagementdate), 'dd.MM.yyyy') as engagementDate
 
 from dbo.engagement_data
